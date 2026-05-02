c881437f6c94a95b13bf3c3832afe7239df60040
Szypkoo <sssszypko@gmail.com>
2026-05-02 12:18:04 +0200

    feat: wire ProcessConditionalsTask into plugin, auto-register per source set

--- src/main/java/me/szypko/codeweaver/CodeWeaverPlugin.java
+++ src/main/java/me/szypko/codeweaver/CodeWeaverPlugin.java
@@ -1,12 +1,55 @@
 package me.szypko.codeweaver;
 
 import me.szypko.codeweaver.extension.Extension;
+import me.szypko.codeweaver.task.ProcessConditionalsTask;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.tasks.SourceSetContainer;
 
 public class CodeWeaverPlugin implements Plugin<Project> {
   @Override
   public void apply(Project target) {
-    target.getExtensions().create("codeWeaver", Extension.class);
+    final Extension extension = target.getExtensions().create("codeWeaver", Extension.class);
+
+    target
+        .getPlugins()
+        .withId(
+            "java",
+            ignored -> {
+              final SourceSetContainer sourceSets =
+                  target.getExtensions().getByType(SourceSetContainer.class);
+
+              sourceSets.configureEach(
+                  sourceSet -> {
+                    final String taskName =
+                        "processConditionals" + capitalize(sourceSet.getName()) + "Sources";
+
+                    target
+                        .getTasks()
+                        .register(
+                            taskName,
+                            ProcessConditionalsTask.class,
+                            task -> {
+                              task.getSourceDirs()
+                                  .from(sourceSet.getAllJava().getSourceDirectories());
+                              task.getFlags().set(extension.getFlags());
+                            });
+
+                    target
+                        .getTasks()
+                        .named(
+                            sourceSet.getCompileJavaTaskName(),
+                            compileJava -> compileJava.dependsOn(taskName));
+                  });
+            });
+  }
+
+  /* I know, I should put this in some kind of util instead of keeping it there. */
+  private static String capitalize(final String string) {
+    if (string.isEmpty()) {
+      return string;
+    }
+
+    return Character.toUpperCase(string.charAt(0)) + string.substring(1);
   }
 }
--- src/test/java/me/szypko/codeweaver/CodeWeaverPluginTest.java
+++ src/test/java/me/szypko/codeweaver/CodeWeaverPluginTest.java
@@ -1,13 +1,11 @@
 package me.szypko.codeweaver;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -15,8 +13,9 @@ class CodeWeaverPluginTest {
   @TempDir File projectDir;
 
   @Test
-  void taskProcessesConditionalBlocks() throws IOException {
+  void pluginRegistersTaskAndProcessesFiles() throws IOException {
     final Path srcDir = projectDir.toPath().resolve("src/main/java");
+
     Files.createDirectories(srcDir);
     Files.writeString(
         srcDir.resolve("Foo.java"),
@@ -31,72 +30,21 @@ class CodeWeaverPluginTest {
         projectDir.toPath().resolve("build.gradle"),
         """
             plugins {
+                id 'java'
                 id 'me.szypko.codeweaver'
             }
             codeWeaver {
                 flag 'TEST_FLAG', false
             }
-            tasks.register('processConditionals', me.szypko.codeweaver.task.ProcessConditionalsTask) {
-                sourceDirs.from(file('src/main/java'))
-                flags = project.extensions.getByType(me.szypko.codeweaver.extension.Extension).getFlags()
-            }
         """);
 
     GradleRunner.create()
         .withProjectDir(projectDir)
-        .withArguments("processConditionals")
+        .withArguments("processConditionalsMainSources")
         .withPluginClasspath()
         .build();
 
-    String result = Files.readString(srcDir.resolve("Foo.java"));
-    assertTrue(result.contains("// function();"));
-  }
-
-  @Test
-  void pluginApplies() throws IOException {
-    Files.writeString(projectDir.toPath().resolve("settings.gradle"), "");
-    Files.writeString(
-        projectDir.toPath().resolve("build.gradle"),
-        """
-            plugins {
-                id 'me.szypko.codeweaver'
-            }
-        """);
-
-    GradleRunner.create()
-        .withProjectDir(projectDir)
-        .withArguments("tasks")
-        .withPluginClasspath()
-        .build();
-  }
-
-  @Test
-  void extensionFlagsAreReadable() throws IOException {
-    Files.writeString(projectDir.toPath().resolve("settings.gradle"), "");
-    Files.writeString(
-        projectDir.toPath().resolve("build.gradle"),
-        """
-            plugins {
-                id 'me.szypko.codeweaver'
-            }
-            codeWeaver {
-                flag 'DEV_MODE', true
-            }
-            tasks.register('printFlags') {
-                doLast {
-                    def flags = project.extensions.getByType(me.szypko.codeweaver.extension.Extension).getFlags()
-                    println "FLAG:" + flags
-                }
-            }
-        """);
-
-    final BuildResult result =
-        GradleRunner.create()
-            .withProjectDir(projectDir)
-            .withArguments("printFlags")
-            .withPluginClasspath()
-            .build();
-
-    assertTrue(result.getOutput().contains("FLAG:[DEV_MODE:true]"));
+    final String result = Files.readString(srcDir.resolve("Foo.java"));
+    Assertions.assertTrue(result.contains("// function();"));
   }
 }
